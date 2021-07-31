4e785b232ac47f4c1ee83921db158b0fd8c8d6e8
Nelson Musto <mustonelson@gmail.com>
2021-07-31 19:02:41 -0300

    Mapa de contacto

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,15 @@
 /*Navigation */
-menuBtn = document.querySelector('nav .button')
-menuLst = document.querySelector('nav .menu')
+    menuBtn = document.querySelector('nav .button')
+    menuLst = document.querySelector('nav .menu')
 
-menuBtn.onclick = () => menuLst.classList.toggle('active') ?
-menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+    menuBtn.onclick = () => menuLst.classList.toggle('active') ?
+    menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
+
+/*Mapa Google Maps*/ 
+    map = document.querySelector('#footer iframe.bg-content')
+    btn = document.querySelector('#footer .top')
+
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('active')
+        map.classList.toggle('active') ? btn.innerHTML = '-' : btn.innerHTML = '+'}
+    )
